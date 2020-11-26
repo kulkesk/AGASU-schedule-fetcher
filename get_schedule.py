f89803363dd6c5e1fedb5909130548a304e27b44
kulkesk <andrcc.on@gmail.com>
2020-11-26 21:00:53 +0400

    FIX

--- get_schedule.py
+++ get_schedule.py
@@ -5,9 +5,8 @@ import datetime as dt
 import urllib.request as urll
 # from pprint import pprint as pp
 from operator import methodcaller
-from urllib.error import URLError
+# from urllib.error import URLError
 from argparse import ArgumentParser as ap
-
 
 week_days_rus = [
     "Понедельник",
@@ -19,23 +18,21 @@ week_days_rus = [
     "Воскресенье\nя ёбнусь если эти бляди посмеют потрогать моё воскресенье"
 ]
 
-
 Pair = tp.Dict[str, tp.Union[str, dt.date, int]]  # Формат в котором возвращаются данные об парах
 # для большей информации об работе библиотеке typing прошу посмотреть соответствующую документацию
 
 
-def get_next_days_after(date: dt.datetime,
+def get_next_days_after(date: dt.date,
                         schedule: tp.Dict[dt.date, Pair],
                         deadline: dict,
                         count: int
                         ):
-    if date > dt.datetime.today().replace(**deadline) and date not in schedule:
-        key = sorted(list([date_schedule for date_schedule in schedule.keys()
-                           if date_schedule - date > dt.timedelta()]))[:count]
-    else:
-        key = date
-    schedule = {key: schedule[key]}
-    return schedule
+    keys = sorted(list([date_schedule for date_schedule in schedule.keys()
+                        if date_schedule - date >= dt.timedelta()]))[:count]
+    schedule_new = {}
+    for key in keys:
+        schedule_new.update({key: schedule[key]})
+    return schedule_new
 
 
 def pretty_print_days(days: tp.Dict[dt.date, Pair]):
@@ -65,7 +62,7 @@ def grouping_by_days(schedule: tp.List[Pair]) -> tp.Dict[dt.datetime, Pair]:
     Группирует уроки по дате
     Возвращает данные по шаблону: {dt.datetime: [Pair, ...], ...]
     """
-    lessons_grouped_by_days = {}  # {dt.datetime: [lesson1, lesson2, lesson3]}
+    lessons_grouped_by_days: tp.Dict[dt.datetime, Pair] = {}  # {dt.datetime: [lesson1, lesson2, lesson3]}
 
     # заполняем дни уроками:
     for lesson in schedule:
@@ -92,21 +89,21 @@ def remove_extra_spaces(text: str) -> str:
     return ' '.join(text.split())
 
 
-def get_schedule_from_server() -> tp.List[Pair]:
+def get_schedule_from_server() -> tp.Union[tp.List[Pair], bool]:
     """
     Получаем данные с сервера АГАСУ и форматируем данные в нужные нам объекты для более лёгкой работы
     """
 
-    url = "https://api.xn--80aai1dk.xn--p1ai/api/schedule?"\
-        "range=3&subdivision_cod=2&group_name=4562"
+    url = "https://api.xn--80aai1dk.xn--p1ai/api/schedule?" \
+          "range=3&subdivision_cod=2&group_name=4562"
 
-    try:
-        response = urll.urlopen(url)
-    except URLError:
-        return False
-
-    if response.code != 200:
-        return False
+    # try:
+    response = urll.urlopen(url)
+    # except URLError:
+    #     return False
+    #
+    # if response.code != 200:
+    #     return False
 
     response_text = response.read().decode("utf-8")
     schedule = json.loads(response_text)  # list[dict[str, str]]
@@ -130,7 +127,6 @@ def get_schedule_from_server() -> tp.List[Pair]:
 
 
 def main():
-
     parser = ap(description="Вывод расписания на эту и следующую неделю")
     parser.add_argument("--next_day", "-N",
                         action="store_true",
@@ -150,17 +146,16 @@ def main():
     schedule = grouping_by_days(schedule)
 
     if display_only_next_day:
-
-        schedule = get_next_days_after(dt.datetime.now(), schedule.copy(), {"hour": 13, "minute": 55}, 1)
+        schedule = get_next_days_after(dt.date.today(), schedule.copy(), {"hour": 13, "minute": 55}, 1)
 
         # now = dt.date.today()
         # TODO: найти способ как можно автоматизировать ну или дать возможность для изменения переменной end_of_last_pair
         # end_of_last_pair = {"hour": 13, "minute": 55}
         # if dt.datetime.now() > dt.datetime.today().replace(**end_of_last_pair)\
-           # and now not in schedule:
-            # key = sorted(list([date for date in schedule.keys() if date - now > dt.timedelta()]))[0]
+        #    and now not in schedule:
+        #     key = sorted(list([date for date in schedule.keys() if date - now > dt.timedelta()]))[0]
         # else:
-            # key = now
+        #     key = now
         # schedule = {key: schedule[key]}
 
     # сортируем пары по дням и выводим после в виде красивой таблицы:
