1c20fd1e186279336fd55d0b7d136c5f400ae200
Kulkesk <andrcc.on@gmail.com>
2021-03-31 21:53:31 +0400

    added some features

--- get_schedule.py
+++ get_schedule.py
@@ -3,6 +3,7 @@ import json
 import typing as tp
 import datetime as dt
 import urllib.request as urll
+import urllib.parse as uparse
 # from pprint import pprint as pp
 from operator import methodcaller
 # from urllib.error import URLError
@@ -19,11 +20,13 @@ week_days_rus = [
 ]
 
 Pair = tp.Dict[str, tp.Union[str, dt.date, int]]  # Формат в котором возвращаются данные об парах
+
+
 # для большей информации об работе библиотеке typing прошу посмотреть соответствующую документацию
 
 
 def get_next_days_after(date: dt.date,
-                        schedule: tp.Dict[dt.date, Pair],
+                        schedule: tp.Dict[dt.date, tp.List[Pair]],
                         count: int
                         ):
     keys = sorted(list([date_schedule for date_schedule in schedule.keys()
@@ -34,7 +37,7 @@ def get_next_days_after(date: dt.date,
     return schedule_new
 
 
-def pretty_print_days(days: tp.Dict[dt.date, Pair]):
+def pretty_print_days(days: tp.Dict[dt.date, tp.List[Pair]]):
     """
     Выводит пары в виде таблицы
     """
@@ -56,12 +59,12 @@ def pretty_print_days(days: tp.Dict[dt.date, Pair]):
             print()
 
 
-def grouping_by_days(schedule: tp.List[Pair]) -> tp.Dict[dt.datetime, Pair]:
+def grouping_by_days(schedule: tp.List[Pair]) -> tp.Dict[dt.datetime, tp.List[Pair]]:
     """
     Группирует уроки по дате
     Возвращает данные по шаблону: {dt.datetime: [Pair, ...], ...]
     """
-    lessons_grouped_by_days: tp.Dict[dt.datetime, Pair] = {}  # {dt.datetime: [lesson1, lesson2, lesson3]}
+    lessons_grouped_by_days: tp.Dict[dt.datetime, tp.List[Pair]] = {}  # {dt.datetime: [lesson1, lesson2, lesson3]}
 
     # заполняем дни уроками:
     for lesson in schedule:
@@ -88,24 +91,65 @@ def remove_extra_spaces(text: str) -> str:
     return ' '.join(text.split())
 
 
-def get_schedule_from_server() -> tp.Union[tp.List[Pair], bool]:
+def get_data_from_server(options: tp.Tuple[str, tp.Dict[str, int]] =
+                         ("schedule", {"range": 3, "subdivision_cod": 2, "group_name": 4562})
+                         ):
+    """
+    Получаем информацию с api сервера
+    """
+    url = f"https://api.xn--80aai1dk.xn--p1ai/api/{options[0]}?{uparse.urlencode(options[1])}"
+    response = urll.urlopen(url)
+    response_text = response.read().decode("utf-8")
+    return json.loads(response_text) if response_text else response_text
+
+
+def get_list_of_options(*, get_list_of_subdivisions: bool = False, groups_in_subdivision_under_id: int = None):
+    """
+    Получение списка доступных опций для запросов к серверу.
+
+    Возвращаются данные в виде словаря с опциями которые можно подать в качестве аргументов к функции
+    get_schedule_from_server
+
+    Ключи словаря это имена аргументов для подачи в get_schedule_from_server
+
+    Значения (которые находятся под ключами) являются словарями чьи ключи обозначают смысл данных а значения ключей
+    обозначают данные которые надо передавать в функцию get_schedule_from_server
+    """
+
+    def reformat(raw):
+        done_ones = {}
+        for item in raw:
+            done_ones[remove_extra_spaces(item["title"])] = item["id"]
+        return done_ones
+
+    options = dict(
+        range={"Эта неделя": 1, "Следующая неделя": 2, "Эта и следующая недели": 3},
+        subdivision_cod={},
+        group_name={},
+    )
+
+    # получаем список учреждений АГАСУ, Строительный колледж, и т.д.
+    if get_list_of_subdivisions:
+        options["subdivision_cod"] = reformat(get_data_from_server(("subdivisions", {})))
+
+    # получаем список учебных групп в учереждении id которого мы задали, если мы задали ему значение
+    if groups_in_subdivision_under_id is not None:
+        options["group_name"] = reformat(
+            get_data_from_server(("groups", {"subdivision_cod": groups_in_subdivision_under_id}))
+        )
+
+    return options
+
+
+def get_schedule_from_server(**options) -> tp.Union[tp.List[Pair], bool]:
     """
     Получаем данные с сервера АГАСУ и форматируем данные в нужные нам объекты для более лёгкой работы
     """
 
-    url = "https://api.xn--80aai1dk.xn--p1ai/api/schedule?" \
-          "range=3&subdivision_cod=2&group_name=4562"
+    if not options:
+        options = {"range": 3, "subdivision_cod": 2, "group_name": 4562}
 
-    # try:
-    response = urll.urlopen(url)
-    # except URLError:
-    #     return False
-    #
-    # if response.code != 200:
-    #     return False
-
-    response_text = response.read().decode("utf-8")
-    schedule = json.loads(response_text)  # list[dict[str, str]]
+    schedule = get_data_from_server(("schedule", options))
 
     for lesson in schedule:
         for key, value in lesson.items():
